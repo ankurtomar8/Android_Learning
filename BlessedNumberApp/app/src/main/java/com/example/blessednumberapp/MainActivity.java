802a3061ffacdeca49b6448fe2c2935a620bfb33
AnkurDev <ankurtomar8@outlook.com>
2024-02-11 17:02:20 +0530

    Added few Search and Home in Menu Layout

--- BlessedNumberApp/app/src/main/java/com/example/blessednumberapp/MainActivity.java
+++ BlessedNumberApp/app/src/main/java/com/example/blessednumberapp/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.blessednumberapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,5 +45,24 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.my_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemId  = item.getItemId();
+
+        if(itemId == R.id.action_home){
+            Toast.makeText(this, "You Selected Home", Toast.LENGTH_LONG).show();
+        }
+        else if(itemId == R.id.action_search){
+            Toast.makeText(this, "You Selected Search", Toast.LENGTH_LONG).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
